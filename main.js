7607fe0b5cdbd1dfbe6e96d0c0cd5f5d9817b8e2
Borntokilltheworld <77736929+Borntokilltheworld@users.noreply.github.com>
2022-02-23 13:17:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects = [];
 video = "";
 status = "";
 function setup(){
@@ -6,6 +7,20 @@ canvas.center();
 }
 function draw(){
     image(video,0,0,480,380)
+    if(status != ""){
+        objectDetector.detect(video,gotResult);
+        for(i = 0;i <objects.length;i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected = "+objects.length;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + "" + percent + "%",objects[i].x,objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+   
 }
 function preload(){
     video = createVideo('video.mp4');
@@ -21,4 +36,13 @@ function ModelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResult(error,result){
+if(error){
+    console.log(error)
+}
+else{
+    console.log(results)
+    objects = results;
+}
 }
